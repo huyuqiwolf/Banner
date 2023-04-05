7dd870b6d5fa7bb30dc893ba32f2e2b29036c380
huyuqiwolf <huyuqiwolf@users.noreply.github.com>
2023-04-05 22:24:49 +0800

    viewpager 实现banenr 轮播

--- app/src/main/java/com/hlox/android/banner/VPBannerActivity.kt
+++ app/src/main/java/com/hlox/android/banner/VPBannerActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.hlox.android.banner.adapter.MyVpAdapter
 import com.hlox.android.banner.data.DataStore
 import com.hlox.android.banner.databinding.ActivityVpBinding
+import com.hlox.android.vpbanner.VPBanner
 
 class VPBannerActivity : AppCompatActivity() {
     private val mBinding by lazy {
@@ -21,5 +22,24 @@ class VPBannerActivity : AppCompatActivity() {
         }
 
         mBinding.vpBanner.resetLoop()
+        mBinding.btnStartLoop.setOnClickListener {
+            mBinding.vpBanner.startLoop()
+        }
+        mBinding.btnStopLoop.setOnClickListener {
+            mBinding.vpBanner.stopLoop()
+        }
+        mBinding.btnSave.setOnClickListener {
+            val orientation = when (mBinding.chipGroup.checkedChipId) {
+                R.id.cp_ltr -> VPBanner.LoopOrientation.LTR
+                else -> VPBanner.LoopOrientation.RTL
+            }
+            val duration = if (mBinding.etDuration.text.trim().toString().isNotEmpty()) {
+                Integer.parseInt(mBinding.etDuration.text.trim().toString())
+            } else 5000
+            mBinding.vpBanner.setLoopDuration(duration.toLong())
+            mBinding.vpBanner.setLoopOrientation(orientation)
+        }
+        lifecycle.addObserver(mBinding.vpBanner)
     }
+
 }
--- vpbanner/src/main/java/com/hlox/android/vpbanner/VPBanner.kt
+++ vpbanner/src/main/java/com/hlox/android/vpbanner/VPBanner.kt
@@ -1,10 +1,14 @@
 package com.hlox.android.vpbanner
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.util.AttributeSet
 import android.util.Log
-import android.view.animation.Interpolator
-import android.view.animation.LinearInterpolator
+import android.view.MotionEvent
+import androidx.annotation.IntDef
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.viewpager.widget.ViewPager
 
 /**
@@ -13,15 +17,40 @@ import androidx.viewpager.widget.ViewPager
  * @date:2023/3/16$
  * @email:huyuqiwolf@163.com
  */
-class VPBanner : ViewPager {
+class VPBanner : ViewPager, DefaultLifecycleObserver {
     companion object {
         private const val TAG = "VPBanner"
         private const val DEFAULT_DURATION = -1
+        private const val LOOP_NEXT = 0x1000
+        private const val DEFAULT_LOOP_DURATION = 5000
+    }
+
+    @IntDef(LoopOrientation.RTL, LoopOrientation.LTR)
+    annotation class LoopOrientation {
+        companion object {
+            /**
+             * 下一张为左侧
+             */
+            const val RTL = 0
+
+            /**
+             * 下一张为右侧
+             */
+            const val LTR = 1
+        }
     }
 
     var loop = false
     private var mLoop: VPLoop? = null
     private var mDuration = DEFAULT_DURATION
+
+    private var mLoopHandler: Handler? = null
+    private var mLoopDuration: Long = DEFAULT_LOOP_DURATION.toLong()
+
+    @LoopOrientation
+    private var mLoopOrientation: Int = LoopOrientation.LTR
+    private var mAutoLoop = false
+    private var mResumed = false
 
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
@@ -29,9 +58,95 @@ class VPBanner : ViewPager {
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.VPBanner)
         this.loop = typedArray.getBoolean(R.styleable.VPBanner_vp_loop, false)
         this.mDuration = typedArray.getInt(R.styleable.VPBanner_vp_duration, DEFAULT_DURATION)
+
+        this.mLoopDuration = typedArray.getInt(
+            R.styleable.VPBanner_vp_loop_duration,
+            DEFAULT_LOOP_DURATION
+        ).toLong()
+        this.mAutoLoop = typedArray.getBoolean(R.styleable.VPBanner_vp_auto_loop, false)
+        this.mLoopOrientation =
+            typedArray.getInt(R.styleable.VPBanner_vp_loop_orientation, LoopOrientation.LTR)
+
+        Log.d(TAG, "ld:${this.mLoopDuration},al:$mAutoLoop,lo:$mLoopOrientation")
+
         typedArray?.recycle()
         setDuration(mDuration)
+        // 如果配置了自动轮播就需要执行自动轮播
+        if (this.mAutoLoop) {
+            startLoop()
+        }
     }
+
+
+    /**
+     * 自动轮播
+     */
+    fun startLoop() {
+        if (mLoopHandler == null) {
+            mLoopHandler = Handler(Looper.getMainLooper()) { message ->
+                return@Handler when (message.what) {
+                    LOOP_NEXT -> {
+                        loopNext()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
+        mLoopHandler?.removeMessages(LOOP_NEXT)
+        mLoopHandler?.sendEmptyMessageDelayed(LOOP_NEXT, mLoopDuration)
+    }
+
+    /**
+     * 设置轮播时长，有效数据必须大于0，否则使用默认数据5S
+     * @param duration Long
+     */
+    fun setLoopDuration(duration: Long) {
+        if (duration < 0) {
+            // 小于0的数据认为是非法数据，使用默认设置
+            return
+        }
+        this.mLoopDuration = duration
+    }
+
+    /**
+     * 设置轮播方向，默认[LoopOrientation.LTR],下一张为右侧
+     * @param orientation Int
+     */
+    fun setLoopOrientation(@LoopOrientation orientation: Int) {
+        this.mLoopOrientation = orientation
+    }
+
+    fun stopLoop() {
+        mLoopHandler?.removeMessages(LOOP_NEXT)
+    }
+
+    private fun loopNext() {
+        val count = adapter?.count ?: 0
+        // 当pager数量为0或者1时，不用轮播
+        if (count in 0..1) return
+        val curr = when (mLoopOrientation) {
+            LoopOrientation.RTL -> {
+                when (currentItem) {
+                    in 1..count - 1 -> {
+                        currentItem - 1
+                    }
+                    else -> count - 1 // 0
+                }
+            }
+            else -> {
+                when (currentItem) {
+                    in 0..count - 2 -> {
+                        currentItem + 1
+                    }
+                    else -> 0 // count - 1
+                }
+            }
+        }
+        setCurrentItem(curr, true)
+        mLoopHandler?.sendEmptyMessageDelayed(LOOP_NEXT, mLoopDuration)
+    }
+
 
     fun setDuration(time: Int) {
         if (time == DEFAULT_DURATION) {
@@ -63,5 +178,33 @@ class VPBanner : ViewPager {
 
     fun cancelLoop() {
         mLoop?.let { removeOnPageChangeListener(it) }
+    }
+
+    override fun onResume(owner: LifecycleOwner) {
+        Log.d(TAG, "onResume")
+        this.mResumed = true
+        prepareLoop()
+    }
+
+    override fun onPause(owner: LifecycleOwner) {
+        Log.d(TAG, "onResume")
+        this.mResumed = false
+        stopLoop()
+    }
+
+    override fun onTouchEvent(ev: MotionEvent?): Boolean {
+        when (ev?.action) {
+            MotionEvent.ACTION_DOWN -> stopLoop()
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                prepareLoop()
+            }
+        }
+        return super.onTouchEvent(ev)
+    }
+
+    private fun prepareLoop() {
+        if (this.mAutoLoop && this.mResumed) {
+            startLoop()
+        }
     }
 }
